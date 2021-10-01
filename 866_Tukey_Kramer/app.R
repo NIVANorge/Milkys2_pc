9a3935ef0ef6fc673021ce636b6c06629d50bc92
Dag_Hjermann <dhj@niva.no>
2021-10-01 09:12:53 +0200

    Update app with ggplot variant of HSD

--- 866_Tukey_Kramer/app.R
+++ 866_Tukey_Kramer/app.R
@@ -8,9 +8,8 @@
 #
 
 library(shiny)
-
+library(ggplot2)
 library(dplyr)
-# library(ggplot2)
 library(agricolae)
 
 # Data and station metadata
@@ -103,21 +102,66 @@ server <- function(input, output) {
         model <- aov(log_Conc ~ Station, data = dat_select())
         outHSD <-  HSD.test(model, "Station", alpha = 0.05)
     })
-
+    
+    df_summ <- reactive({
+        multcomp_val <- multcomp()
+        multcomp_val$means %>%
+            mutate(Station = rownames(multcomp_val$means)
+            ) 
+    })
+    
+    # For plotting Min, Max, Q25, Q75, Station_group labels
+    df_groups <- reactive({
+        multcomp_val <- multcomp()
+        data.frame(
+            Station = rownames(multcomp_val$groups),
+            Station_group = multcomp_val$groups$groups) %>%
+            left_join(df_summ(), by = "Station") %>%
+            rename(
+                Mean = log_Conc,
+                Median = Q50)
+    })
+    
+    # For plotting mean and median
+    df_groups2 <- reactive({
+        df_groups_val <- df_groups()
+        df_groups_val %>%
+            select(Station, Mean, Median) %>%
+            tidyr::pivot_longer(-Station, names_to = "Type", values_to = "Value")
+    })
+    
     output$ggplot <- renderPlot({
         ggplot(dat_select(), aes(Station, VALUE_WW, color = LOQ)) +
             geom_jitter(width = 0.1, height = 0, size = rel(1.6)) +
             scale_color_manual(values = c("Under LOQ" = "red2", "Over LOQ" = "black")) +
             scale_y_log10() +
-            theme(axis.text.x = element_text(angle = -45, hjust = 0, size = rel(1.5)))
+            theme(axis.text.x = element_text(angle = -45, hjust = 0, size = rel(1.5))) +
+            labs(x = "")
     })
+    
+    # output$multcomp_plot <- renderPlot({
+    #     par(mfrow=c(1,1), mar=c(3,10,2,3))
+    #     plot(multcomp(), horiz=TRUE, las = 1, cex.names = 1, 
+    #          main = "Tukey-Kramer HSD test")
+    # })
     
     output$multcomp_plot <- renderPlot({
-        par(mfrow=c(1,1), mar=c(3,10,2,3))
-        plot(multcomp(), horiz=TRUE, las = 1, cex.names = 1, 
-             main = "Tukey-Kramer HSD test")
+        dat_select_val <- dat_select()
+        ggplot(df_groups2(), aes(Station)) +
+            geom_linerange(data = df_groups(), aes(ymin = Min, ymax = Max)) +
+            geom_linerange(data = df_groups(), aes(ymin = Q25, ymax = Q75), size = 1.25) +
+            geom_point(aes(y = Value, shape = Type), color = "white", fill = "red2", size = 3.5) +
+            scale_shape_manual(values = c(22, 23)) +
+            geom_text(data = df_groups(),
+                      aes(
+                          y = Max + diff(range(dat_select_val$log_Conc))*0.07,
+                          label = Station_group,
+                          color = Station_group)) +
+            labs(title = "Tukey-Kramer HSD post-hoc test", 
+                 y = "log10(Concentration + 0.1)", x = "") +
+            theme(axis.text.x = element_text(angle = -45, hjust = 0, size = rel(1.5)))
+            
     })
-    
     
     
 }
