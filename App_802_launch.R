41e1c514e4e2bb0b3240235ba436bf5587469a53
Dag_Hjermann <dhj@niva.no>
2022-01-02 08:25:53 +0100

    Add launcher files for apps
    - usage decribed in script
    - canot be used with 'system()' - you need to get the gate given by the server process

---
+++ App_802_launch.R
@@ -0,0 +1,15 @@
+#
+# Run by opening a terminal/CMD window in the folder Milkys2_pc
+# Then run 
+# R --vanilla <App_802_launch.R &
+#
+# The last line of the resonse should be "Listening on ...", e.g. http://127.0.0.1:6178
+# In the browser, go to the given web adress, e.g. http://127.0.0.1:6178/ 
+#
+# The app is run by an R proess in the background. To stop it, push Esc button. 
+#
+# From 
+# https://stackoverflow.com/a/34317976
+
+library(shiny)
+runApp("App_802")
