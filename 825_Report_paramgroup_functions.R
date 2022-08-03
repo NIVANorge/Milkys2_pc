48aa899094399dbef10f2a9b3e5c6a04f2fee779
Dag_Hjermann <dhj@niva.no>
2022-08-03 10:42:41 +0200

    Functioning tooltip version
    - without text and EQS marking

--- 825_Report_paramgroup_functions.R
+++ 825_Report_paramgroup_functions.R
@@ -334,58 +334,48 @@ if (F){
 }
 
 
-pargroup_median_table_tooltip <- function(data_medians, fill, year){
+pargroup_median_table_tooltip <- function(data_medians, fill, year,
+                                          width_svg = 6, height_svg = 3.5){
   
   data_medians$fill <- data_medians[[fill]]
   
-  fill_min <- floor(1000*min(data_medians$fill, na.rm = T))/1000
-  fill_max <- ceiling(max(data_medians$fill, na.rm = T))
-  
-  # if (!is.null(tooltip)){
-  #   data_medians$tooltip <- data_medians[[tooltip]]
-  # } else {
-  #   data_medians$tooltip <- with(data_medians, paste(Station2, PARAM, ":", round(VALUE_WW_med, 3)))
-  # }
+  fill_column <- fill
   
   dat_plot <- data_medians %>%
     filter(MYEAR %in% year) %>%
-    arrange(desc(PARAM)) %>%
+    arrange(desc(PARAM))
+  
+  # fill_min <- floor(1000*min(dat_plot$fill, na.rm = T))/1000
+  # fill_max <- ceiling(max(dat_plot$fill, na.rm = T))
+  fill_min <- 0.0001
+  fill_max <- 1000
+  
+  dat_plot <- dat_plot %>% 
     mutate(
-      PARAM = forcats::fct_inorder(PARAM),
-      fill = cut(fill, breaks = c(fill_min,1,2,3,5,10,fill_max)),
-      VALUE_WW_txt = paste("Median:", round(VALUE_WW_med, 4), " ug/kg<br>", 
-                           "(", round(VALUE_WW_min, 4), "-", round(VALUE_WW_max, 4), ")")
-  )
+      fill_cut = cut(fill, breaks = c(fill_min,1,2,3,5,10,fill_max)),
+      VALUE_WW_txt = paste0(
+        fill_column, ": ", round(fill, 3), "<br>",
+        "Median: ", round(VALUE_WW_med, 4), " ug/kg<br>",
+        "(", round(VALUE_WW_min, 4), "-", round(VALUE_WW_max, 4), ")")) %>%
+    select(Proref_ratio_WW, VALUE_WW_txt, MYEAR, Station2, PARAM, fill, fill_cut)
   
   cols <- c(RColorBrewer::brewer.pal(6, "Blues")[2],
             RColorBrewer::brewer.pal(6, "YlOrRd")[1:5])
   names(cols) <- levels(dat_plot$fill)
   
-  gg <- ggplot(dat_plot, aes(Station2, PARAM, fill = fill)) +
-    geom_tile()
-  gg <- gg +
-    geom_tile(data = subset(dat_plot, Above_EQS %in% "Over"),
-              color = "red", size = 1, height = 0.9, width = 0.9) +
-    geom_text(aes(label = round(VALUE_WW_med, 3)), nudge_y = -0.1, size = 3) +
-    geom_text(aes(label = LOQ_label), size = 3, nudge_y = 0.3) +
-    #scale_fill_viridis_b(trans = "log10", breaks = c(0.01,1,2,3,5,10,100), option = "plasma") +
-    #scale_fill_binned(breaks = c(0.01,1,2,3,5,10,100)) +
-    scale_fill_manual(breaks = c(0.01,1,2,3,5,10,100), values = cols) +
-    scale_color_manual(values = c("red", "white")) +
-    scale_alpha_manual(values = c(1, 0)) +
+  p <- ggplot(dat_plot, aes(Station2, PARAM, tooltip = VALUE_WW_txt)) + 
+    geom_tile_interactive(aes(fill = fill_cut)) +
+    scale_fill_manual(fill_column, values = cols) +
     scale_y_discrete() +
     theme_bw() +
     theme(axis.text.x = element_text(angle = -45, hjust = 0))
-  labs(
-    title = "Medians"
-  )
   
-  gg
+  girafe(ggobj = p, height_svg = 3)
   
 }
 
 if (F){
-  debugonce(pargroup_median_table_tooltip)
+  # debugonce(pargroup_median_table_tooltip)
   pargroup_median_table_tooltip(dat_median_fish, fill = "Proref_ratio_WW", year = 2021)
 }
 
