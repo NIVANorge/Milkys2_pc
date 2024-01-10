7a55253577b5a01a12753e7c49d34a74b9a5e8e4
Dag_Hjermann <dhj@niva.no>
2024-01-10 15:21:53 +0100

    app 809: add comments
    Also expands on the test script

--- Apps/App_809/app.R
+++ Apps/App_809/app.R
@@ -3,7 +3,7 @@
 #
 
 #
-# Code to perform when app is starting
+# Code performed when app is starting ----
 #
 
 library(shiny)
@@ -16,40 +16,53 @@ library(maps)
 library(mapdata)
 simple_map <- map_data("worldHires", c("Norway"))
 
+# Station coordinates
+# IN TEST SCRIPT: path changed (../.. removed) 
 dat_coord <- read_excel("../../Files_for_other_use/Milkys_stasjoner_for_kart.xlsx") %>%
   select(STATION_CODE, Lat, Long, Station_name)
 
+# Some of the stations are very close together and it's impossible to click a single station
+# Therefore, we move some points, and connect them to the original points using a line
+# (this is just an example, this should be done also for other clusters of points - see test script)
 dat_coord_moved <- tibble::tribble(
   ~STATION_CODE, ~Lat, ~Long,
   "30A", 61.5, 15,
   "30B", 61, 15,
   "I301", 60.5, 15,
   "I304", 60, 15,
-  "I306", 59.5, 15,
-  "I307", 59, 15,
-  "31A", 58.5, 15,
+  "31A", 59.5, 15,
 ) 
 
+# Modify the data so we end up with two columns for the original coordinates (_orig),
+#   and Long and Lat are modified for staations given by 'dat_coord_moved'  
+# Both sets of coordinates will be used when plotting the map 
+#   (black dot for the orginal coordinaytes, coloured dot for new coordinates, and a line between)
+# Clicking on the *moved* points in the map affects 
 dat_coord <- dat_coord %>%
   rename(
     Lat_orig = Lat, 
     Long_orig = Long) %>%
+  # New ("moved") coordinates are added as new columns  
   left_join(dat_coord_moved, by = "STATION_CODE") %>%
+  # Where there are no new coordinates given by 'dat_coord_moved', the original coordinates will be used
   mutate(
     Lat = ifelse(is.na(Lat), Lat_orig, Lat),
     Long = ifelse(is.na(Long), Long_orig, Long)
   ) 
 
+# Median concentrations (median per year per time series)
 dat_med <- readRDS("../../Files_from_Jupyterhub_2020/Raw_data/110_mediandata_updated_2021-10-08.rds") %>%
+  # 5 columns (old Coordinates, moved coordinates and Station_name) are added  
   left_join(dat_coord, by = "STATION_CODE") %>%
+  # Keep only data for which we have coordinates 
   filter(!is.na(Lat))
 
+# Stations (with coordinates) that are found in the data since 2020  
 dat_stations <- dat_med %>%
   filter(MYEAR == 2020) %>%
   distinct(STATION_CODE, Lat_orig, Long_orig, Lat, Long, Station_name, LATIN_NAME) %>%
   # For now we just delete Svalbard (to make the map fit better)
   filter(Lat_orig < 72)
-  # filter(Lat > 65 & Lat < 68)
 
 # Parameters to choose from (only those found in 2020)
 params_avaliable <- dat_med %>%
@@ -62,7 +75,7 @@ basis_avaliable <- dat_med %>%
   pull(Basis)
 
 #
-# User interface  
+# User interface ----  
 #
 ui <- fluidPage(
 
@@ -72,32 +85,38 @@ ui <- fluidPage(
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
       sidebarPanel(
+        # Menu for selecting parameter (compound) 
         selectizeInput(inputId = "params_selected", label = "Parameters", 
                        choices = params_avaliable, multiple = TRUE,
                        selected = c("CB118", "CB138")),
+        # Menu for selecting basis   
         selectizeInput(inputId = "basis_selected", label = "Basis", 
                        choices = basis_avaliable, multiple = FALSE,
                        selected = "WW"),
+        # Menus only affecting the plot     
         sliderInput("min_x", "Show plot from year", min = 1980, max = 2020, value = 1990, sep = "", step = 1),
         numericInput("max_y", "Max y axis", 0),
         width = 4
       ),
       
-      # Show a plot of the generated distribution
+      # Show plots
       mainPanel(
+        # Map
         plotOutput("map", height = "400px", click = "plot_click"), 
+        # Timne series plot(s)  
         plotOutput("timeseriesplot", height = "300px"), 
-        verbatimTextOutput("table"),
         width = 8
         )
     )
 )
 
 #
-# Server code  
+# Server code ---- 
 #
 server <- function(input, output) {
   
+  # stations_available() is a reactive function that returns
+  #   the stations that are available with the given selection of PARAM
   stations_avaliable <- reactive({
     dat_med %>%
       filter(MYEAR >= 2020, 
@@ -112,26 +131,40 @@ server <- function(input, output) {
     dat_stations_available <- dat_stations %>% 
       filter(STATION_CODE %in% stations_avaliable())
     ggplot(dat_stations_available, aes(Long, Lat)) +
+      # Background map of Norway
       annotation_map(simple_map, fill = "lightgreen") +
+      # Actual station position (black dot)
       geom_point(aes(x = Long_orig, y = Lat_orig)) +
+      # Line from actual station position to station label
       geom_segment(aes(x = Long_orig, y = Lat_orig, xend = Long, yend = Lat)) +
-      geom_point(aes(color = LATIN_NAME)) +
-      geom_text(aes(label = STATION_CODE), hjust = 0, vjust = 0.25, nudge_x = 0.35) 
+      # Coloured station dot - this is the one that should be clicked
+      geom_point(aes(color = LATIN_NAME), size = 2) +
+      # Station code (to the right of the coloured station dot)  
+      geom_text(aes(label = STATION_CODE), hjust = 0, vjust = 0.25, nudge_x = 0.35) +
+      # Keep proportions (hight/width) of the map:
+      coord_fixed(ratio = 2) +
+      theme_bw()
+      # The map would look better iwith e.g. Lambert projection, but that messes up 
+      #   click interaction:
       # coord_map("lambert", parameters = c(10.4, 59.3))
   }) 
   
   output$timeseriesplot <- renderPlot({
     dat_plot_param <- dat_med %>%
+      # Select rows by parameter and basis
       filter(PARAM %in% input$params_selected,
              Basis == input$basis_selected) %>%
+      # For colouring points by LOQ status 
       mutate(
         Over_LOQ_perc = round(Over_LOQ/N_median*100,0),
         Over_LOQ_med = ifelse(Over_LOQ_perc >= 50, ">=50% over LOQ", "<50% over LOQ")
       )
+    # Select rows by station (by clicking on the map)
     dat_plot <- nearPoints(dat_plot_param, 
                            input$plot_click, 
                            xvar = "Long", yvar = "Lat", threshold = 5)   
     if (nrow(dat_plot) > 0){
+      # Make ggplot
       gg <- ggplot(dat_plot, aes(MYEAR, Value)) +
         geom_line() +
         geom_point(aes(shape = Over_LOQ_med, fill = Over_LOQ_perc), size = 2) +
@@ -139,6 +172,7 @@ server <- function(input, output) {
         scale_fill_viridis_b() +
         facet_grid(vars(STATION_CODE), vars(PARAM))
       if (input$max_y != 0){
+        # Set limits
         gg <- gg + 
           coord_cartesian(xlim = c(input$min_x, 2020), ylim = c(0, input$max_y))
       }
--- Apps/App_809/test_app809.R
+++ Apps/App_809/test_app809.R
@@ -1,3 +1,8 @@
+
+#
+# Getting data ----
+#
+
 library(shiny)
 library(dplyr)
 library(ggplot2)
@@ -7,23 +12,85 @@ library(maps)
 library(mapdata)
 simple_map <- map_data("worldHires", c("Norway"))
 
-dat_med <- readRDS("Files_from_Jupyterhub_2020/Raw_data/110_mediandata_updated_2021-10-08.rds")
+# Station coordinates
+# IN TEST SCRIPT: path changed (../.. removed) 
 dat_coord <- read_excel("Files_for_other_use/Milkys_stasjoner_for_kart.xlsx") %>%
   select(STATION_CODE, Lat, Long, Station_name)
 
-df_stations <- dat_med %>%
-  count(STATION_CODE) %>%
-  left_join(dat_coord) %>%
+# Some of the stations are very close together and it's impossible to click a single station
+# Therefore, we move some points, and connect them to the original points using a line
+# (this is just an example, this should be done also for other clusters of points)
+dat_coord_moved <- tibble::tribble(
+  ~STATION_CODE, ~Lat, ~Long,
+  "I301", 62, 15,
+  "30A",  61.5, 15,
+  "I304", 61, 15,
+  "30B",  60.5, 15,
+  "31A",  60, 15,
+) 
+
+# Modify the data so we end up with two columns for the original coordinates (_orig),
+#   and Long and Lat are modified for staations given by 'dat_coord_moved'  
+# Both sets of coordinates will be used when plotting the map 
+#   (black dot for the orginal coordinaytes, coloured dot for new coordinates, and a line between)
+# Clicking on the *moved* points in the map affects 
+dat_coord <- dat_coord %>%
+  rename(
+    Lat_orig = Lat, 
+    Long_orig = Long) %>%
+  # New ("moved") coordinates are added as new columns  
+  left_join(dat_coord_moved, by = "STATION_CODE") %>%
+  # Where there are no new coordinates given by 'dat_coord_moved', the original coordinates will be used
+  mutate(
+    Lat = ifelse(is.na(Lat), Lat_orig, Lat),
+    Long = ifelse(is.na(Long), Long_orig, Long)
+  ) 
+
+# Median concentrations (median per year per time series)
+# IN TEST SCRIPT: path changed (../.. removed) 
+dat_med <- readRDS("Files_from_Jupyterhub_2020/Raw_data/110_mediandata_updated_2021-10-08.rds") %>%
+  # 5 columns (old Coordinates, moved coordinates and Station_name) are added  
+  left_join(dat_coord, by = "STATION_CODE") %>%
+  # Keep only data for which we have coordinates 
   filter(!is.na(Lat))
 
-# For now we just delete Svalbard
-df_stations <- df_stations %>%
-  filter(Lat < 72)
+# Stations (with coordinates) that are found in the data since 2020  
+dat_stations <- dat_med %>%
+  filter(MYEAR >= 2020) %>%
+  distinct(STATION_CODE, Lat_orig, Long_orig, Lat, Long, Station_name, LATIN_NAME) %>%
+  # For now we just delete Svalbard (to make the map fit better)
+  filter(Lat_orig < 72)
+# filter(Lat > 65 & Lat < 68)
 
-# Map from library(maps)
-ggplot(df_stations, aes(Long, Lat)) +
+#
+# Map ----
+#
+
+# IN TEST SCRIPT: let all stations are available 
+# In the app, stations_available() is a reactive function that returns
+#   which stations that are available with the give selection of PARAM
+dat_stations_available <- dat_stations 
+#  filter(STATION_CODE %in% stations_avaliable())
+
+gg <- ggplot(dat_stations_available, aes(Long, Lat)) +
   annotation_map(simple_map, fill = "lightgreen") +
-  geom_point() +
-  coord_map("lambert", parameters = c(10.4, 59.3))
-  
-  
+  geom_point(aes(x = Long_orig, y = Lat_orig)) +
+  geom_segment(aes(x = Long_orig, y = Lat_orig, xend = Long, yend = Lat)) +
+  geom_point(aes(color = LATIN_NAME)) +
+  geom_text(aes(label = STATION_CODE), hjust = 0, vjust = 0.25, nudge_x = 0.35) 
+gg
+
+#
+# Stations to put in 'dat_coord_moved'
+#
+
+# These stations have been added to dat_coord_moved:
+gg + coord_cartesian(xlim = c(10,15), ylim = c(59.3, 60))
+# These stations should be added to dat_coord_moved:
+gg + coord_cartesian(xlim = c(10,15), ylim = c(59.3, 60) - 0.7)     # outer Oslo fjord
+gg + coord_cartesian(xlim = c(6.5,8.5), ylim = c(57.5, 58.5))       # South coast
+gg + coord_cartesian(xlim = c(7.75,8.25), ylim = c(58, 58.25))      # South coast, detail
+gg + coord_cartesian(xlim = c(5, 9), ylim = c(59, 61))              # West coast
+gg + coord_cartesian(xlim = c(5, 6), ylim = c(60.25, 60.5))         # West coast, detail
+gg + coord_cartesian(xlim = c(11, 17), ylim = c(66, 69))            # Nordland  
+gg + coord_cartesian(xlim = c(14.6, 15.1), ylim = c(68.15, 68.27))  # Nordland, detail  
